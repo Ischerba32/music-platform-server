bcf1081f084485f46e76c2735d9770b6bb53c30c
Ischerba32 <i.scherba32@gmail.com>
2022-05-03 11:26:22 +0300

    added comment to track, searching, File entity, static serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
-
+import * as path from 'path';
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     MongooseModule.forRoot('mongodb://localhost:27017/music-service'),
     TrackModule,
+    FileModule,
   ],
 })
 export class AppModule {}
